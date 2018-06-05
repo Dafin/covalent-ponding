96885988de5ee12caf8f6a05773311592061416f
Dafin <dafin@users.noreply.github.com>
2018-06-05 16:59:48 +0100

    - Refactored multiple button rendering to be components, frogspot defined as having a letter attribute. Pre - ES6 way in comments

--- App.js
+++ App.js
@@ -3,25 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native';
 
 
+// Pre ES6...
+// var FrogSpot = function(props) {
+//   return (
+//     <Button
+//       onPress={function() { alert('hello ' + props.letter) }}
+//       title={props.letter}
+//       color="#841584"
+//     />
+//   );
+// };
+
+const FrogSpot = ({letter}) => (
+  <Button
+    onPress={() => alert(`hello ${letter}`)}
+    title={letter}
+    color="#841584"
+  />
+);
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button
-          onPress={function() { alert("hello") }}
-          title="p"
-          color="#841584"
-        />
-        <Button
-          onPress={function() { alert("hello") }}
-          title="e"
-          color="#841584"
-        />
-        <Button
-          onPress={function() { alert("hello") }}
-          title="n"
-          color="#841584"
-        />
+        <FrogSpot letter="p" />
+        <FrogSpot letter="e" />
+        <FrogSpot letter="n" />
       </View>
     );
   }
