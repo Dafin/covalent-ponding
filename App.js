e0c6ce79bc10497d5c52951f20d36714cc083d12
Dafin <dafin@users.noreply.github.com>
2018-06-05 22:18:31 +0100

    - Added lodash library for chaining methods - Added initial game configuration

--- App.js
+++ App.js
@@ -1,39 +1,111 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {chain, filter, map} from 'lodash';
 
-const FrogSpot = (props) => {
-  const {letter, hasFrog, isOnBank} = props;
-
+const Spot = ({frog, hasFrog, isCorrect, type}) => {
   return (
     <View style={styles.spot}>
       <Button
-        onPress={() => alert(`I am: ${letter}`)}
-        title={letter}
+        onPress={() => alert(`I am: ${frog}`)}
+        title={frog || ''}
         color="#841584"
       />
     </View>
   );
 };
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      spots: [
+        {
+          frog: 'e',
+          hasFrog: true,
+          type: 'bank'
+        },
+        {
+          frog: 'p',
+          hasFrog: true,
+          type: 'bank'
+        },
+        {
+          frog: 'n',
+          hasFrog: true,
+          type: 'bank'
+        },
+        {
+          frog: null,
+          isCorrect: 'p',
+          hasFrog: false,
+          type: 'pond'
+        },
+        {
+          frog: null,
+          isCorrect: 'e',
+          hasFrog: false,
+          type: 'pond'
+        },
+        {
+          frog: null,
+          isCorrect: 'n',
+          hasFrog: false,
+          type: 'pond'
+        }
+      ]
+    };
+  }
+
   render() {
+    const { spots } = this.state;
+
+    console.log(spots);
+
     return (
       <View style={styles.container}>
+
         <View style={styles.pond}>
-          <FrogSpot letter="p" hasFrog={false} isOnBank={false} />
-          <FrogSpot letter="e" hasFrog={false} isOnBank={false} />
-          <FrogSpot letter="n" hasFrog={false} isOnBank={false} />
+          { chain(spots)
+            .filter(({type}) => type === 'pond')
+            .map(({frog, hasFrog, isCorrect, type}, i) => (
+              <Spot
+                key={i}
+                frog={frog}
+                hasFrog={hasFrog}
+                isCorrect={isCorrect}
+                type={type}
+              />
+            ))
+            .value()
+          }
         </View>
 
         <View style={styles.bank}>
-          <FrogSpot letter="p" hasFrog={true} isOnBank={true} />
-          <FrogSpot letter="e" hasFrog={true} isOnBank={true} />
-          <FrogSpot letter="n" hasFrog={true} isOnBank={true} />
+          { chain(spots)
+            .filter(({type}) => type === 'bank')
+            .map(({frog, hasFrog, isCorrect, type}, i) => (
+              <Spot
+                key={i}
+                frog={frog}
+                hasFrog={hasFrog}
+                isCorrect={isCorrect}
+                type={type}
+              />
+            ))
+            .value()
+          }
         </View>
       </View>
     );
   }
 }
+
+         // <FrogSpot letter="p" hasFrog={true} isOnBank={true} />
+         //  <FrogSpot letter="e" hasFrog={true} isOnBank={true} />
+         //  <FrogSpot letter="n" hasFrog={true} isOnBank={true} />
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -51,6 +123,8 @@ const styles = StyleSheet.create({
   },
   spot: {
     borderWidth: 1,
+    minHeight: 50,
+    minWidth: 50,
     margin: 1
   }
 });
